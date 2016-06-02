9c1786975e65da80180140a9e829c09831420659
Cleyton Fábio <pixies@pelivre.org>
2016-06-02 01:59:56 -0300

    customizando area de profile do administrador

--- membro_profile/admin.py
+++ membro_profile/admin.py
@@ -2,5 +2,9 @@ from membro_profile.models import MembroProfile, ComprovantePagamento
 from django.contrib import admin
 from membro_profile.models import MembroProfile, ComprovantePagamento
 # Register your models here.
-admin.site.register(MembroProfile)
-admin.site.register(ComprovantePagamento)
+class MembroProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('user','cpf', 'status_inscricao','tipo_de_inscricao','data_de_cadastro')
+
+admin.site.register(MembroProfile, MembroProfileAdmin)
+#admin.site.register(ComprovantePagamento)
