99e5fea816b0c18e182127103d4e2ad9d53835e6
Cleyton Fábio <pixies@pelivre.org>
2016-06-02 01:28:27 -0300

    DEBUG

--- academic/settings.py
+++ academic/settings.py
@@ -21,9 +21,9 @@ with open('/etc/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
