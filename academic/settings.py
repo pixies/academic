47d13517735e2b4a9761932e02087fd7bd70efc3
Cleyton Fábio <pixies@pelivre.org>
2016-06-02 00:38:47 -0300

    corrigindo secret_key

--- academic/settings.py
+++ academic/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i!^a77%_$yhvcfqj2khtf@ncm(ygtum%pbe%2fg((alhwnc3ya'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
