6636ec83b30312010dfd60410a81cb5a7e07aee0
Cleyton Fábio <pixies@pelivre.org>
2016-06-03 01:19:46 -0300

    apresentacao admin

--- submissao/admin.py
+++ submissao/admin.py
@@ -3,5 +3,6 @@ from submissao.models import Submissao
 class SubmissaoAdmin(admin.ModelAdmin):
 
     list_display = ('titulo','autor','data_de_criacao','area','status_do_resumo')
+
 # Register your models here.
 admin.site.register(Submissao, SubmissaoAdmin)
