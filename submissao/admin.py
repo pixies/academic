0fa1209dd955d3e3de970f7367e6ba0ab7c0413c
Cleyton Fábio <pixies@pelivre.org>
2016-06-04 10:12:50 -0300

    Removendo a aplicação de submissao

--- submissao/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from submissao.models import Submissao
-class SubmissaoAdmin(admin.ModelAdmin):
-
-    list_display = ('titulo','autor','data_de_criacao','area','status_do_resumo')
-
-# Register your models here.
-admin.site.register(Submissao, SubmissaoAdmin)
