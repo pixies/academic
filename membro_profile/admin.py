353b177de071d09c2675569de5178432e3dd8b75
Cleyton Fábio <pixies@pelivre.org>
2016-06-05 18:15:55 -0300

    Adcionando edicao rapida

--- membro_profile/admin.py
+++ membro_profile/admin.py
@@ -1,11 +1,11 @@
-from membro_profile.models import MembroProfile, ComprovantePagamento
 from django.contrib import admin
-from membro_profile.models import MembroProfile, ComprovantePagamento
+from membro_profile.models import MembroProfile
 # Register your models here.
 class MembroProfileAdmin(admin.ModelAdmin):
 
     list_display = ('user','cpf', 'status_inscricao','tipo_de_inscricao','data_de_cadastro')
     list_filter = ['status_inscricao','tipo_de_inscricao']
+    list_editable = ['status_inscricao']
 
 admin.site.register(MembroProfile, MembroProfileAdmin)
 #admin.site.register(ComprovantePagamento)
