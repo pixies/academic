d102eaf6d61e605a1863fc7a1913067bfec228ca
Cleyton Fábio <pixies@pelivre.org>
2016-06-06 12:02:38 -0300

    50%

---
+++ submissao/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect
+from submissao.models import Submissao
+from submissao.forms import SubmissaoForm
+
+# Create your views here.
+def adcionar_submissao(request):
+    form = SubmissaoForm(request.POST or None, request.FILES or None)
+#    submissao = Submissao.objects.filter(autor_id=request.user.membroprofile.id)
+    submissao = False
+
+    if submissao == False:
+        if form.is_valid():
+            instance = form.save(commit=False)
+            print instance
+            instance.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/submissao')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'submissao/nova_submissao.html', context)
+
+def listar_submissao(request):
+
+    queryset = Submissao.objects.filter(autor_id=request.user.membroprofile.id)
+
+    if request.user.is_authenticated():
+        context = {
+            'lista_resumos': queryset,
+            "title": 'Minha Lista'
+        }
+    else:
+        context = {
+            "title": 'Lista'
+        }
+    return render(request, 'submissao/index.html', context)
+
+def detalhes_submissao(request):
+    context = {
+        "title": 'Detalhes'
+    }
+    return render(request, 'submissao/index.html',context)
+
+def editar_submissao(request):
+    queryset = Submissao.objects.filter(autor_id=request.user.membroprofile.id)
+
+    if request.user.is_authenticated():
+
+        form = SubmissaoForm(request.POST or None)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+
+        context = {
+            "lista_resumos": queryset,
+            "title": 'Minha Lista',
+            "form": form
+        }
+    else:
+        context = {
+            "title": 'Lista'
+        }
+
+    return render(request, 'submissao/index.html', context)
+
+
+
+
+
+def deletar_submissao(request):
+    return HttpResponse('deletar')
